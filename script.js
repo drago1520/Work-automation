4dcf88739940294596dab42b6d081cb8549243c6
drago1520 <dragomir1520@gmail.com>
2023-12-08 17:24:59 +0200

    Opens all urls, adds %%page%%, closes properly. (wait for navigation to finish + delay of 1 sec.)

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ import "dotenv/config";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 puppeteer.use(StealthPlugin());
-import "dotenv/config";
+
 
 async function main(){
-  async function fetchSheetData() {
-    const spreadsheetId = '1nD5gnRe7WnIXtvsF5M-hBU390TqhHApIeGEo0k7ydwA';
+  
+  //const urls = await fetchSheetData(spreadSheetID);
+  async function fetchSheetData(spreadSheetID) {
+    const spreadsheetId = spreadSheetID;
     const sheetName = 'Sheet1';
-    const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${GOOGLE_SHEET_API_FACEBOOK}`;
+    const SheetsAPIFacebook = process.env.GOOGLE_SHEET_API_FACEBOOK;
+    const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${SheetsAPIFacebook}`;
     let flatArrayURL = [];
     
     try {
@@ -22,63 +25,107 @@ async function main(){
     }
     return flatArrayURL;
   }
-  
-  
-  async function facebookGroups() {
-    const urls = await fetchSheetData();
-    console.log(urls);
-    
-      
-      //# executablePath: "<от къде да отвори google?>", userDataDir: "<къде се намира userDataDir>"
-      const browser = await puppeteer.launch({headless: false, executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe', userDataDir: 'C:\\Users\\Dell\\AppData\\Local\\Google\\Chrome\\User Data'  });
-      
-      //# i = първият URL - 1.  (обяснение: 1-ви елемент в Google Sheets = 0, защото програмистите броят от 0,1,2,3,4,...)
-      for (let i = 0; i < urls.length; i++) { 
-  
-        const url = urls[i];
-        const page = await browser.newPage();
-        console.log(url);
-        try{
-          console.log(`Opening ${url}`);
-          await page.goto(url, { waitUntil: 'networkidle0' });
-          await page.waitForXPath('//*[text()="Напишете нещо..."]', { visible: true });
-          let elements = await page.$x('//*[text()="Напишете нещо..."]');
-          let NewPostButton_1 = elements[0]; // assuming it's the first element matching the XPath
-          await NewPostButton_1.click();
-          await NewPostButton_1.focus();
+  //
+  async function openNewPage(url, browser){
+    try{
+      const page = await browser.newPage();
+      await page.goto(url, { waitUntil: 'networkidle0' });
+      return page;
+    }catch(error){
+      console.log(error);
+    }
+  }
+  //const <areaName> = await selectXpath(<path>);
+  async function selectXpath(path, page){
+    try{
+      await page.waitForXPath(path, { visible: true });
+          let elements = await page.$x(path);
+          let NewPostButton_1 = elements[0];
+          if(NewPostButton_1){
+            return NewPostButton_1;
+          }else{
+            console.log('Element not found in selectXpath()');
+          }
+    }catch(error){
+      console.log(error);
+    }
           
-          // Wait for the element to be visible
-          await page.waitForXPath('//span[contains(text(), "Публикуване")]', { visible: true });
-          //Ctrl + V
-          await page.keyboard.down('Control');
+  }
+
+  //
+  async function clickElement(element){
+    if(element){
+      await element.click();
+    }else{
+      console.log('Element not found in clickElement()');
+    }
+  }
+  //
+  async function delay (ms){
+    await new Promise(resolve => setTimeout(resolve, ms));
+  }
+  //const page = await openBrowser();
+  async function openBrowser(){
+    try{
+      const exePath = 'C:/Program Files/Google/Chrome/Application/chrome.exe';
+      const userDataGeneral = 'C:\\Users\\Dell\\AppData\\Local\\Google\\Chrome\\User Data';
+      const browser = await puppeteer.launch({headless: false, executablePath: exePath, userDataDir: userDataGeneral  });
+      
+      return browser;
+    }catch(e){
+      console.log(e);
+    }
+  }
+  async function closePage (){
+    try{
+      await page.close();
+    }catch(e){
+      console.log(e);
+    }
+  }  
+async function Pagenation(){
+  const spreadsheetId = '1YxcrNipNI3l8GWzz-p8mb4-IUvmmAg7f2ZyyDaAxSIU'; // Your Spreadsheet ID
+  const urls = await fetchSheetData(spreadsheetId);
+  const browser = await openBrowser();
+for (let i = 0; i<urls.length; i++){
+  console.log("Opening url: ", urls[i]);
+  const page = await openNewPage(urls[i], browser);
+  const editButton = await selectXpath("//li[@id='wp-admin-bar-edit']/a", page);
+  await clickElement(editButton);
+  await page.waitForXPath("//div[@id='yoast-google-preview-description-metabox']", { visible: true });
+  console.log("loaded");
+  const metaDescriptionBox = await selectXpath("//div[@id='yoast-google-preview-description-metabox']", page);
+  await metaDescriptionBox.focus();
+  await page.keyboard.down('Control'); // Hold down the 'Control' key
+  await page.keyboard.press('End'); // Press the 'End' key while 'Control' is held down
+  await page.keyboard.up('Control'); // Release the 'Control' key
+  await page.keyboard.press('Space');
+  await page.keyboard.type('%%page%%');
+  const saveButton = await selectXpath("//div[@class='edit-tag-actions']/input[@type='submit']", page);
+  //Just to check.
+  await delay(4000);
+  const navigationPromise = page.waitForNavigation()
+  await clickElement(saveButton);
+  await navigationPromise;
+  await page.close();
   
-          // Press the 'V' key
-          await page.keyboard.press('V');
+  console.log(`${urls[i]} modified successfully!`);
+  }}
   
-          // Release the Control key
-          await page.keyboard.up('Control');
-          let SubmitButton_2Array = await page.$x('//span[contains(text(), "Публикуване")]');
-          let SubmitButton_2 = SubmitButton_2Array[0];
-          await new Promise(resolve => setTimeout(resolve, 2000));
-          await SubmitButton_2.click();
-          console.log("Posted on url: " + url);
-          await new Promise(resolve => setTimeout(resolve, 10000)); //Да зареди post submit... Може още оптимизация. Сега чака 6 сек. да се submit-не
-          await page.close();
-          await new Promise(resolve => setTimeout(resolve, 2000)); //След колко време да отвори другия URL; #сложи рандом сек. забавяне м/у всяко действие
-        }catch(error){
-          console.log(error);
-          console.log("Probably the facebook page is not accesible");
-          console.log("Did not post on url: " + url);
-          await page.close();
-          await new Promise(resolve => setTimeout(resolve, 2000)); //След колко време да отвори другия URL; #сложи рандом сек. забавяне м/у всяко действие
-        }}
-      console.log("All urls have been posted!");
-      }
-   
-  //# Should it post on Facebook groups? - to comment out the function place //facebookGroups().catch(console.error); / to run it place facebookGroups().catch(console.error);
-  //facebookGroups().catch(console.error);
-  
-  
+Pagenation().catch(console.error);
+
+//Успешно добави на БГ. #провери и другите за 
+
+
+
+
+
+
+
+
+
+
+
   
 }
 main().catch(console.error);
